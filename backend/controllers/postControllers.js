b733905a3a284d9026b5fdfa01ce3915952f26dc
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 21:20:24 +0400

    adding controller

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,2 +1,25 @@
 import Post from "../models/postModels.js";
 
+export const getPosts = async (req,res) => {
+    try{
+        const posts = await Post.find()
+        return res.status(200).json({posts})
+    }catch(error){
+        return res.status(500).json({error: error.message})
+    }
+}
+
+export const createPost = async (req,res)=>{
+    const {title,body} = req.body;
+
+    if(!title || !body){
+        return res.status(400).json({error: "All fields are required"})
+    }
+
+    try{
+        const post = await Post.create({title, body})
+        res.status(201).json({success: "Post created", post})
+    }catch(error){
+        return res.status(500).json({error: error.message})
+    }
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,29 +1,8 @@
 import express from 'express'
-import Post from '../models/postModels.js';
+import { createPost, getPosts } from '../controllers/postControllers.js';
 const router = express.Router()
 
-router.get("/", async (req,res) => {
-    try{
-        const posts = await Post.find()
-        return res.status(200).json({posts})
-    }catch(error){
-        return res.status(500).json({error: error.message})
-    }
-})
-
-router.post("/", async (req,res)=>{
-    const {title,body} = req.body;
-
-    if(!title || !body){
-        return res.status(400).json({error: "All fields are required"})
-    }
-
-    try{
-        const post = await Post.create({title, body})
-        res.status(201).json({success: "Post created", post})
-    }catch(error){
-        return res.status(500).json({error: error.message})
-    }
-})
+router.get("/", getPosts)
+router.post("/", createPost)
 
 export default router;
