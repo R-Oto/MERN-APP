b5a03468134a5147a0526875653e0f1abde5bb8e
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 22:51:57 +0400

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
 import User from "../models/userModels.js"
 import bcrypt from 'bcryptjs'
 
+//create jwt
+
+
 export const registerUser = async (req,res) => {
     const {email, password} = req.body;
 
@@ -26,5 +29,25 @@ export const registerUser = async (req,res) => {
 }
 
 export const loginUser = async (req,res) => {
+    const {email, password} = req.body;
 
+    if(!email || !password){
+        return res.status(400).json({error: "All fields are required"})
+    }
+
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(400).json({error: "email is taken"})
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if(!match){
+        return res.status(400).json({error: "Incorrect password"})
+    }
+
+    try{
+        return res.status(200).json({email})
+    }catch(error){
+        return res.status(500).json({error:error.message})
+    }
 }
