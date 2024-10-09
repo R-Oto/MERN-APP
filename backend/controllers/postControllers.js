7aac99eba2b36299b2c0745c481ae81b9ad7e8a7
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 23:28:24 +0400

    backend completed

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 import Post from "../models/postModels.js";
+import User from "../models/userModels.js";
 
 export const getPosts = async (req,res) => {
     try{
         const posts = await Post.find()
         return res.status(200).json({posts})
+    }catch(error){
+        return res.status(500).json({error: error.message})
+    }
+}
+
+export const getUserPosts = async (req,res) => {
+
+    const user = await User.findById(req.user._id)
+
+    try{
+        const userPosts = await Post.find({user: user._id})
+        return res.status(200).json({userPosts})
     }catch(error){
         return res.status(500).json({error: error.message})
     }
@@ -17,8 +30,10 @@ export const createPost = async (req,res)=>{
         return res.status(400).json({error: "All fields are required"})
     }
 
+    const user = await User.findById(req.user._id)
+
     try{
-        const post = await Post.create({title, body})
+        const post = await Post.create({user: user._id, title, body})
         res.status(201).json({success: "Post created", post})
     }catch(error){
         return res.status(500).json({error: error.message})
@@ -33,6 +48,11 @@ export const deletePost = async (req,res)=>{
     
     if (!post){
         return res.status(400).json({error: "Post not found"})
+    }
+
+    const user = await User.findById(req.user._id)
+    if(!post.user.equals(user._id)) {
+        return res.status(401).json({error: "not authorized"})
     }
 
     try{
@@ -58,6 +78,11 @@ export const updatePost = async (req,res)=>{
 
     if(!title || !body){
         return res.status(400).json({error: "All fields are required"})
+    }
+
+    const user = await User.findById(req.user._id)
+    if(!post.user.equals(user._id)) {
+        return res.status(401).json({error: "not authorized"})
     }
 
     try{
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createPost, getPosts, deletePost, updatePost } from '../controllers/postControllers.js';
+import { createPost, getPosts, getUserPosts, deletePost, updatePost } from '../controllers/postControllers.js';
 import auth from '../middlewares/auth.js';
 const postRoutes = express.Router()
 
 postRoutes.get("/", getPosts)
+postRoutes.get("/user", auth, getUserPosts)
 postRoutes.post("/", auth, createPost)
-postRoutes.delete("/:id", deletePost)
-postRoutes.put("/:id", updatePost)
+postRoutes.delete("/:id", auth, deletePost)
+postRoutes.put("/:id", auth, updatePost)
 
 export default postRoutes;
