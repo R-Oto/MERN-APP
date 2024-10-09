2b5ada3a7221268c9bbd5f1896cdf4f42e390118
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 22:42:02 +0400

    user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,24 @@
+import User from "../models/userModels.js"
 
 export const registerUser = async (req,res) => {
-    
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({error: "All fields are required"})
+    }
+
+    const exists = await User.findOne({email})
+    if(exists){
+        return res.status(400).json({error: "email is taken"})
+    }
+
+    try{
+        const user = await User.create({email, password})
+        return res.status(201).json({email})
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({error:error.message})
+    }
 }
 
 export const loginUser = async (req,res) => {
