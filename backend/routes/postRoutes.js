d690c1b48ebcd64a32e265cd4a49ea0c4303e07d
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 23:14:34 +0400

    add middleware

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { createPost, getPosts, deletePost, updatePost } from '../controllers/postControllers.js';
+import auth from '../middlewares/auth.js';
 const postRoutes = express.Router()
 
 postRoutes.get("/", getPosts)
-postRoutes.post("/", createPost)
+postRoutes.post("/", auth, createPost)
 postRoutes.delete("/:id", deletePost)
 postRoutes.put("/:id", updatePost)
 
