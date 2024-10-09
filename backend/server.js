90bd9e886d94dea350afa581920b1fecc8e1e6d6
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-09 21:08:15 +0400

    setup PostSchema

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import router from './routes/postRoutes.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use("/api/posts", router)
 
 mongoose.connect(process.env.ATLAS).then(()=>{
     console.log("Connected")
